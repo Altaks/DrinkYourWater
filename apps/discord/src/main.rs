f710760cd125e5aa57eef1ff3594689009f8c9c7
Altaks <altair61.dev@gmail.com>
2025-07-31 11:10:14 +0200

    sync: basic bot command

--- apps/discord/src/main.rs
+++ apps/discord/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error::Error};
+
+use serenity::{all::Message, async_trait, prelude::*};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenvy::dotenv()?;
+
+    let token = env::var("DISCORD_BOT_TOKEN")?;
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await?;
+
+    if let Err(reason) = client.start_autosharded().await {
+        println!("Client error while starting : {:?}", reason);
+    }
+
+    Ok(())
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
 }
