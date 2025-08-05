a193d1d1648482c4b1acf9c3ea4d0cc21fe9721c
Aubin Perez-Roldan <aubinperezroldan@gmail.com>
2025-08-05 19:40:38 +0200

    feat: Improvement of random messages
    - Add error message
    - Correction of spelling mistakes
    - Converting multiline messages to raw &str
    - Fix random utilisation

--- apps/discord/src/data/messages.rs
+++ apps/discord/src/data/messages.rs
@@ -3,60 +3,60 @@ pub const REMINDER_MESSAGE_THIRTY_MIN: [&str; 10] = [
     "💧 C'est l'heure de boire un verre d'eau à moitié plein 💧",
     "💧 C'est l'heure de boire un verre d'eau à moitié vide 💧",
     "💧 C'est l'heure de boire un peu d'eau, et pas autre chose 👀 💧",
-    "🐟 : bl.oupb.loup.upb bloup.b.upblo 
+    r"🐟 : bl.oupb.loup.upb bloup.b.upblo 
 🤖 : Le poisson te demande de boire de l'eau",
     "💧 Il est l'heuurree, l'heure de boire un peu d'eaaauuu 💧",
     "💧 Il est temps que tu boives un peu d'eau, non, tes larmes ne comptent pas💧",
-    "💧 C'est l'heure de boire un peu d'eau. Fais attention à ne pas en renverser, l'eau sa mouille 💧",
+    "💧 C'est l'heure de boire un peu d'eau. Fais attention à ne pas en renverser, l'eau ça mouille 💧",
     "💧 C'est l'heure de boire un peu d'eau. Non, manger un glaçon ne compte pas 💧",
     "💧 C'est l'heure de boire un peu d'eau. 💧",
 ];
 
 pub const REMINDER_MESSAGE_ONE_HOUR: [&str; 6] = [
     "💧 C'est l'heure de boire un verre d'eau ! 💧",
-    "Salut. J'ai un cadeau pour toi : 🎁
-    
-    C'est un verre d'eau 🥤 que tu dois boire, car...
-    💧 C'est l'heure de boire un verre d'eau ! 💧",
-    "💧 C'est l'heure de boire un verre d'eau !💧 
-    Pense à vérifier qu'il n'y a pas de poisson dans ton verre avant de boire
-    Sinon, remplie un autre verre d'eau et mets-lee dedans. Tu pourras savourer ton verre d'eau",
+    r"Salut. J'ai un cadeau pour toi : 🎁
+
+C'est un verre d'eau 🥤 que tu dois boire, car...
+💧 C'est l'heure de boire un verre d'eau ! 💧",
+    r"💧 C'est l'heure de boire un verre d'eau !💧 
+Pense à vérifier qu'il n'y a pas de poisson dans ton verre avant de boire
+Sinon, remplis un autre verre d'eau et mets-le dedans. Tu pourras savourer ton verre d'eau",
     "iL et l'eure dè bUvoir unE verrre d'O",
-    "***C’est** le moment, j’abandonne le bruit,
-    **L’heure** calme où le corps se réjouit.
-    **De** mes mains, je saisis la cruche en pierre,
-    **Boire** devient une prière légère.
-    **Un** souffle, une pause, l’instant me serre,
-    **Verre** en main, je savoure la lumière.
-    **D’eau** vive, je nourris ce qu’il me faut —
-    Et tout en moi devient plus haut.*
+    r"***C’est** le moment, j’abandonne le bruit,
+**L’heure** calme où le corps se réjouit.
+**De** mes mains, je saisis la cruche en pierre,
+**Boire** devient une prière légère.
+**Un** souffle, une pause, l’instant me serre,
+**Verre** en main, je savoure la lumière.
+**D’eau** vive, je nourris ce qu’il me faut —
+Et tout en moi devient plus haut.*
 
-    En bref buvez un verre d'eau.
-    ",
-    "Je vous présente Sushi, le poisson : 🐟
-    
-    Savez-vous quel est le point commun entre Sushi et vous ?
+En bref buvez un verre d'eau.",
+    r"Je vous présente Sushi, le poisson : 🐟
 
-    Vous buvez tous les deux de l'eau. D'ailleurs...
-    💧 C'est l'heure de boire un verre d'eau ! 💧
-    "
+Savez-vous quel est le point commun entre Sushi et vous ?
+
+Vous buvez tous les deux de l'eau. D'ailleurs...
+💧 C'est l'heure de boire un verre d'eau ! 💧"
 ];
 
 pub const REMINDER_MESSAGE_THREE_HOURS: [&str; 4] = [
     "💧 C'est l'heure de boire une grande quantité d'eau ! 💧",
-    "Dans des temps immémoriaux, existait un roi qui était surnommé le roi d'eau.
-    Il était aimé de tous ces citoyens pour sa bonté. En effet, il fournissait de manière gratuite et illimitée de l'eau à ces sujets.
-    Cependant, malgré cet accès illimité à l'eau, de nombreux citoyens mourraient de soif. La raison de ces morts était que les citoyens ne boivent pas, car ils n'ont pas soif.
-    Le roi d'eau, en voyant cela, décida de prendre des mesures drastiques. Il créa une division nommée 'les buveurs' ayant pour mission de trouver les 'non assoiffés et de leur faire boire de l'eau par la force.
-    Autrefois paisible, le royaume est devenu un territoire où la peur était omniprésente.
-    D'autant que personne ne connaissait la méthode utilisée par la division pour faire boire les non-assoiffés. 
-    Ce qui était sûr, c'est que les personnes arrêtées boivent bien mieux après leur enlèvement.
+    r"Dans des temps immémoriaux, existait un roi qui était surnommé le roi d'eau.
+Il était aimé de tous ces citoyens pour sa bonté. En effet, il fournissait de manière gratuite et illimitée de l'eau à ces sujets.
+Cependant, malgré cet accès illimité à l'eau, de nombreux citoyens mourraient de soif. La raison de ces morts était que les citoyens ne buvaient pas car ils n'avaient pas soif.
+Le roi d'eau, en voyant cela, décida de prendre des mesures drastiques. Il créa une division nommée 'les buveurs' ayant pour mission de trouver les 'non assoiffés' et de leur faire boire de l'eau par la force.
+Autrefois paisible, le royaume est devenu un territoire où la peur était omniprésente.
+D'autant que personne ne connaissait la méthode utilisée par la division pour faire boire les non-assoiffés. 
+Ce qui était sûr, c'est que les personnes arrêtées boivent bien mieux après leur enlèvement.
     
-    💧 En bref, c'est l'heure de boire une grande quantité d'eau ! Sinon, faites attention aux personnes qui vous suivent💧",
-    "🐟 🐠 🐟 🐠 🐟 🐠 🐟 🐠 🐟 🐠 🐟 🐠 🐟 🐠 🐟 🐠 🐟
+💧 En bref, c'est l'heure de boire une grande quantité d'eau ! Sinon, faites attention aux personnes qui vous suivent💧",
+    r"🐟 🐠 🐟 🐠 🐟 🐠 🐟 🐠 🐟 🐠 🐟 🐠 🐟 🐠 🐟 🐠 🐟
     
-    Oh non, c'est la Fish-Squad, un groupe de poissons très dangereux.
-    Vite ! Buvez une grande quantité d'eau pour vous sauver.
-    ",
+Oh non, c'est la Fish-Squad, un groupe de poissons très dangereux.
+Vite ! Buvez une grande quantité d'eau pour vous sauver.",
     "💧 C'est l'heure de boire une grande quantité d'eau ! Cependant, pas besoin de boire un lac.💧",
 ];
+
+pub const ERROR_MESSAGE: &str = r"Une erreur est survenue. Le bot n'a pas bu assez d'eau.
+D'ailleurs, pensez à boire pour ne pas finir comme lui.";
